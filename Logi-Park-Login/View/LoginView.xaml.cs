e1bf0f5ad6123dbf618157205c35e291b7f2ba96
Hangsihak Sin <Hsin9104@conestogac.on.ca>
2024-01-10 20:09:43 -0500

    Add project files.

---
+++ Logi-Park-Login/View/LoginView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Logi_Park_Login.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void UsernameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Username or email")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = System.Windows.Media.Brushes.Black; 
+            }
+        }
+
+        private void UsernameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "Username or email";
+                textBox.Foreground = System.Windows.Media.Brushes.Gray; 
+            }
+        }
+
+        private void PasswordTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Password")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = System.Windows.Media.Brushes.Black; 
+            }
+        }
+
+        private void PasswordTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "Password";
+                textBox.Foreground = System.Windows.Media.Brushes.Gray; 
+            }
+        }
+    }
+}
