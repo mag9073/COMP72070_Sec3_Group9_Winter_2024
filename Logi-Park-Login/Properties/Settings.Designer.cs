7bcee7c3c0ca2cb77e6f0db93269f55c5a6ce248
hsihak <hsin9104@conestogac.on.ca>
2024-03-01 11:50:01 -0500

    add: implement Program Client Class

--- Logi-Park-Login/Properties/Settings.Designer.cs
+++ Logi-Park-Login/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Client.Properties {
+namespace LogiPark.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
