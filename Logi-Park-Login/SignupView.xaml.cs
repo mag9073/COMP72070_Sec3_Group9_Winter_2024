fbfaf4a02b68f9171d06bdd23ce7bdd2520fef0d
CovOwe04 <owenacov@gmail.com>
2024-03-05 14:04:41 -0500

    Fixed navigation errors and changed the navigation system a bit to flow nicely

---
+++ Logi-Park-Login/SignupView.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LogiPark
+{
+    /// <summary>
+    /// Interaction logic for SignupView.xaml
+    /// </summary>
+    public partial class SignupView : Window
+    {
+        public SignupView()
+        {
+            InitializeComponent();
+        }
+
+        private void UsernameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Username or email")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void UsernameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "Username or email";
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void PasswordTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Password")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void PasswordTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "Password";
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = true;
+            StringBuilder sb = new StringBuilder();
+
+            // Define username and password -> get it from textbox (check xaml code)
+
+            // Verify Username -- This can be separated to a helper method
+            if (string.IsNullOrWhiteSpace(usernameTextBox.Text) || usernameTextBox.Text == "Username or email")
+            {
+                isValid = false;
+                sb.AppendLine("Please enter your username or email");
+            }
+
+            if (string.IsNullOrWhiteSpace(passwordTextBox.Text) || passwordTextBox.Text == "Password")
+            {
+                isValid = false;
+                sb.AppendLine("Please enter your password");
+            }
+
+            if (!isValid)
+            {
+                MessageBox.Show(sb.ToString(), "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                // If valid then open a home view
+                //HomeView homeView = new HomeView();
+
+                //homeView.Show();
+
+                //this.Close();
+            }
+
+            // If the username and password are filled, then can proceed to sign up process
+
+
+            // Initialize it to the SignUp Data Class
+
+            // Send info to the sign up class 
+
+            // Need to implement a method to send info
+
+            // Need to implement a method to receive data back?
+
+            // its it is good then it can proceed, take them back to the login page and current view 
+
+        }
+
+        private void Login_Handler(object sender, MouseButtonEventArgs e)
+        {
+
+            // Handle login label clicked
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- Server/ImageManager.cs
+++ Server/ImageManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
+//using System.Windows.Media.Imaging;
 
 namespace Server
 {
@@ -11,7 +11,7 @@ namespace Server
     {
         public string FileName;
         public byte[] Data;
-        public BitmapImage Image;   // We will use this reconstruct the image when we receive byte [] image data back from the server
+        //public BitmapImage Image;   // We will use this reconstruct the image when we receive byte [] image data back from the server
 
     }
 }
