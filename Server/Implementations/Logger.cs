9d58ccf3594f83e55c5bcc71da6bb0d34b1ea11e
Hangsihak Sin <hsin9104@conestogac.on.ca>
2024-03-11 18:02:15 -0400

    fix: reorganize Logger class
    Co-Authored-By: Npackullmccormi7020 <113472856+Npackullmccormi7020@users.noreply.github.com>

---
+++ Server/Implementations/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Implementations
+{
+    public class Logger
+    {
+        private DateTime timeOfTransmission;
+        private string logFilePath;
+
+        public Logger(string logFileName)
+        {
+            logFilePath = logFileName;
+        }
+
+        public bool Log(byte[] message)
+        {
+            timeOfTransmission = getTime();
+            string output = Encoding.Default.GetString(message);
+            using (StreamWriter writer = File.AppendText(logFilePath))
+            {
+                writer.WriteLine(timeOfTransmission.ToString() + ": " + output);
+            }
+            return true;
+        }
+
+        private DateTime getTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
