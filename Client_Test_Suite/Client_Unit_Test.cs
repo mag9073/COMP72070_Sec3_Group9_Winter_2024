c22b9b4d3161a4761c9c64239c8de6c56ec6f7a5
umang <umohini4648@conestogac.on.ca>
2024-03-11 10:48:04 -0400

    made a test case and will continue it on my branch

--- Client_Test_Suite/Client_Unit_Test.cs
+++ Client_Test_Suite/Client_Unit_Test.cs
@@ -1,4 +1,5 @@
 //using Client;
+using LogiPark.MVVM.View;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Security.Cryptography.X509Certificates;
 namespace Client_Test_Suite
@@ -7,8 +8,10 @@ namespace Client_Test_Suite
     public class Client_Unit_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UT_CL_001() // should pass
         {
+            //var view = new ClientHomeView();
+            //Assert.IsNotNull(view.Client);
         }
     }
 }
